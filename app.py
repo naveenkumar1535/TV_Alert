85886cb3bde6c5aa939978aee62de34f45c60bd9
naveenkumar1535 <151647484+naveenkumar1535@users.noreply.github.com>
2023-11-21 19:37:02 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    data = request.get_json()
+    print("Received TradingView alert:")
+    print(data)
+    # Add your custom logic to handle the TradingView alert data here
+    return jsonify({'message': 'Webhook received successfully'})
+
+if __name__ == '__main__':
+    app.run(debug=True)
