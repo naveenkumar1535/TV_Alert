1fa03fd95d1372bdebebf5daed6ec46d8667458b
naveenkumar1535 <151647484+naveenkumar1535@users.noreply.github.com>
2023-11-21 20:47:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ app = Flask(__name__)
 
 @app.route('/webhook', methods=['POST'])
 def webhook():
+    print('Inside Code')
     data = request.get_json()
     print("Received TradingView alert:")
     print(data)
