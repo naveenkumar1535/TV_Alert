e98d8ce28e3eac906d8e39fcc0de9135591115de
naveenkumar1535 <151647484+naveenkumar1535@users.noreply.github.com>
2023-11-21 21:31:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,14 +5,24 @@ app = Flask(__name__)
 @app.route('/webhook', methods=['POST'])
 def webhook():
     print('Inside Code')
-    data = request.get_json()
-    print("Received TradingView alert:")
-    print(data)
+    try:
+        data = request.json#.get_json()
+        symbol = data.get('symbol')
+        action = data.get('action')
+        price = data.get('price')
+        timestamp = data.get('timestamp')
+        message = data.get('message')
+        print(f"Received TradingView alert: Symbol={symbol}, Action={action}, Price={price}, Timestamp={timestamp}, Message={message}")
+        print("Received TradingView alert:")
+        print(data)
     # Add your custom logic to handle the TradingView alert data here
-    return jsonify({'message': 'Webhook received successfully'})
+        return jsonify({'message': 'Webhook received successfully'})
+    except Exception as e:
+        print(f"Error processing TradingView alert: {e}")
+        return jsonify({'error': 'Error processing alert'}), 500
 
 def main():
     webhook()
     
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False)
